ce8d1a599d415ad640c7ef0181c65f0ac3acb0e8
“Haotian <haotianliu@g.ucla.edu>
2024-08-11 11:57:41 -0400

    Convert date for comparison

--- app.js
+++ app.js
@@ -6,5 +6,13 @@ document.addEventListener("DOMContentLoaded", () => {
     const destinationName = document.getElementById("destination-name").value;
     const destinationStart = document.getElementById("destination-start").value;
     const destinationEnd = document.getElementById("destination-end").value;
+    // Convert dates to Date objects for comparison
+    const startDate = new Date(destinationStart);
+    const endDate = new Date(destinationEnd);
+
   });
+
+  function addDestination(name, startDate, endDate) {
+
+  }
 });
