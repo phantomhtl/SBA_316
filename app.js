fc67383fb877a6e16f9403033a333bc45a3b6c9b
“Haotian <haotianliu@g.ucla.edu>
2024-08-11 11:54:22 -0400

    Assigned varname for elements

--- app.js
+++ app.js
@@ -3,5 +3,8 @@ document.addEventListener("DOMContentLoaded", () => {
   const destinationList = document.getElementById("destination-list");
   form.addEventListener("submit", (e) => {
     e.preventDefault();
+    const destinationName = document.getElementById("destination-name").value;
+    const destinationStart = document.getElementById("destination-start").value;
+    const destinationEnd = document.getElementById("destination-end").value;
   });
 });
