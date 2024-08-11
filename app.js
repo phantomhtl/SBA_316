2f57d5f2559d35c2e7b1e5fcb60357252f5c1d7d
“Haotian <haotianliu@g.ucla.edu>
2024-08-11 11:53:22 -0400

    Prevent default for form

--- app.js
+++ app.js
@@ -1,4 +1,7 @@
 document.addEventListener("DOMContentLoaded", () => {
   const form = document.getElementById("destination-form");
   const destinationList = document.getElementById("destination-list");
+  form.addEventListener("submit", (e) => {
+    e.preventDefault();
+  });
 });
